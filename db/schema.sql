897d4a876e9248454dd47da65ab585db670d222e
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-10-30 09:37:29 -0500

    mysql error when trying to see table contents

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@ USE team_db;
 
 CREATE TABLE department (
     id INT NOT NULL,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+    name VARCHAR(30)
+    -- PRIMARY KEY (id)
 );
 
 
@@ -15,10 +15,10 @@ CREATE TABLE role (
     id INT NOT NULL,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    department_id INT
+    -- PRIMARY KEY (id),
+    -- FOREIGN KEY (department_id)
+    -- REFERENCES department(id)
 );
 
 
@@ -27,12 +27,12 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id)
-    REFERENCES role(id),
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    manager_id INT
+    -- PRIMARY KEY (id),
+    -- FOREIGN KEY (role_id)
+    -- REFERENCES role(id),
+    -- FOREIGN KEY (manager_id)
+    -- REFERENCES employee(id)
 );
 
 
